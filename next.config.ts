16880bcc523a66f0e039e613b6a527b0115ba634
choycoy <passionfruits479@gmail.com>
2026-03-15 18:37:23 +0900

    fix: updating the Next.js config and package

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   turbopack: {
     root: __dirname,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(tsx|jsx|ts|js)$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            process.env.NODE_ENV === 'production'
-              ? require.resolve('./babel-plugin')
-              : false,
-          ].filter(Boolean),
-        },
-      },
-    });
-    return config;
   },
 };
 
